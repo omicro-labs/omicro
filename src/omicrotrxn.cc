62fe5c211dde3c115d1027d48869ba2ba8ace0fe
wasabiomicro <wasabiomicro@omicro>
2022-03-09 12:18:58 -0800

    added code in omicrotrxn.h omicrotrxn.cc

--- src/omicrotrxn.cc
+++ src/omicrotrxn.cc
@@ -1,10 +1,249 @@
+#include <malloc.h>
+#include <string.h>
+#include <iostream>
 #include "omicrotrxn.h"
 
 OmicroTrxn::OmicroTrxn()
 {
+	data_[TRXN_TOTAL_SZ+1] = '\0';
+}
+
+OmicroTrxn::OmicroTrxn( const char *str )
+{
+	int len = strlen(str);
+	if ( len >= TRXN_TOTAL_SZ ) {
+		memcpy(data_, str, TRXN_TOTAL_SZ);
+	} else {
+		std::cout << "E00001 OmicroTrxn::OmicroTrxn(s) s=[" << str << "] is too short" << std::endl;
+	}
+	data_[TRXN_TOTAL_SZ+1] = '\0';
 }
 
 OmicroTrxn::~OmicroTrxn()
 {
 }
 
+// caller needs to free the pointer
+char* OmicroTrxn::getHeader()
+{
+	int start = TRXN_HEADER_START;
+	int sz = TRXN_HEADER_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setHeader( const char *s)
+{
+	int len = strlen(s);
+	int start = TRXN_HEADER_START;
+	int sz = TRXN_HEADER_SZ;
+	if ( len != sz ) {
+		std::cout << "E10000 OmicroTrxn::setHeader s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+char* OmicroTrxn::getBeacon()
+{
+	int start = TRXN_BEACON_START;
+	int sz = TRXN_BEACON_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setBeacon( const char *s)
+{
+	int start = TRXN_BEACON_START;
+	int sz = TRXN_BEACON_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10001 OmicroTrxn::setBeacon s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+
+char* OmicroTrxn::getSender()
+{
+	int start = TRXN_SENDER_START;
+	int sz = TRXN_SENDER_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setSender( const char *s)
+{
+	int start = TRXN_SENDER_START;
+	int sz = TRXN_SENDER_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10002 OmicroTrxn::setSender s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+char* OmicroTrxn::getReceiver()
+{
+	int start = TRXN_RECEIVER_START;
+	int sz = TRXN_RECEIVER_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setReceiver( const char *s)
+{
+	int start = TRXN_RECEIVER_START;
+	int sz = TRXN_RECEIVER_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10003 OmicroTrxn::setReceiver s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+double OmicroTrxn::getAmountDouble()
+{
+	char *p = getAmount();
+	double f = atof(p);
+	free(p);
+	return f;
+}
+
+char* OmicroTrxn::getAmount()
+{
+	int start = TRXN_AMOUNT_START;
+	int sz = TRXN_AMOUNT_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setAmount( const char *s)
+{
+	int start = TRXN_AMOUNT_START;
+	int sz = TRXN_AMOUNT_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10003 OmicroTrxn::setAmout s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+char* OmicroTrxn::getTimeStamp()
+{
+	int start = TRXN_TIMESTAMP_START;
+	int sz = TRXN_TIMESTAMP_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setTimeStamp( const char *s)
+{
+	int start = TRXN_TIMESTAMP_START;
+	int sz = TRXN_TIMESTAMP_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10004 OmicroTrxn::setTimeStamp s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+
+char* OmicroTrxn::getTrxnType()
+{
+	int start = TRXN_TRXNTYPE_START;
+	int sz = TRXN_TRXNTYPE_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setTrxnType( const char *s)
+{
+	int start = TRXN_TRXNTYPE_START;
+	int sz = TRXN_TRXNTYPE_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10005 OmicroTrxn::setTrxnType s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+char* OmicroTrxn::getAssetType()
+{
+	int start = TRXN_ASSETTYPE_START;
+	int sz = TRXN_ASSETTYPE_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setAssetType( const char *s)
+{
+	int start = TRXN_ASSETTYPE_START;
+	int sz = TRXN_ASSETTYPE_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10006 OmicroTrxn::setAssetType s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+
+char* OmicroTrxn::getSignature()
+{
+	int start = TRXN_SIGNATURE_START;
+	int sz = TRXN_SIGNATURE_SZ;
+	char *p = (char*)malloc(sz+1);
+	memcpy( p, data_+start, sz );
+	p[sz] = '\0';
+	return p;
+}
+
+bool OmicroTrxn::setSignature( const char *s)
+{
+	int start = TRXN_SIGNATURE_START;
+	int sz = TRXN_SIGNATURE_SZ;
+	int len = strlen(s);
+	if ( len != sz ) {
+		std::cout << "E10007 OmicroTrxn::setSignature s=[" << s << "] wrong size" << std::endl; 
+		return false;
+	}
+	memcpy( data_+start, s, sz );
+	return true;
+}
+
+// caller frees the pointer
+char *OmicroTrxn::getString()
+{
+	return strdup(data_);
+}
--- src/omicrotrxn.h
+++ src/omicrotrxn.h
@@ -1,24 +1,91 @@
 #ifndef _omicro_trxn_h_
 #define _omicro_trxn_h_
 
+#define TRXN_HEADER_START  0
+#define TRXN_HEADER_SZ     6
+
+#define TRXN_BEACON_START  TRXN_HEADER_SZ
+#define TRXN_BEACON_SZ     8
+
+#define TRXN_SENDER_START  (TRXN_BEACON_START+TRXN_BEACON_SZ)
+#define TRXN_SENDER_SZ     503
+
+#define TRXN_RECEIVER_START (TRXN_SENDER_START+TRXN_SENDER_SZ)
+#define TRXN_RECEIVER_SZ   503
+
+#define TRXN_AMOUNT_START  (TRXN_RECEIVER_START+TRXN_RECEIVER_SZ)
+#define TRXN_AMOUNT_SZ     16
+
+#define TRXN_TIMESTAMP_START (TRXN_AMOUNT_START+TRXN_AMOUNT_SZ)
+#define TRXN_TIMESTAMP_SZ  16
+
+#define TRXN_TRXNTYPE_START (TRXN_TIMESTAMP_START+TRXN_TIMESTAMP_SZ)
+#define TRXN_TRXNTYPE_SZ   2
+
+#define TRXN_ASSETTYPE_START (TRXN_TRXNTYPE_START+TRXN_TRXNTYPE_SZ)
+#define TRXN_ASSETTYPE_SZ  2
+
+#define TRXN_SIGNATURE_START  (TRXN_ASSETTYPE_START+TRXN_ASSETTYPE_SZ)
+#define TRXN_SIGNATURE_SZ  64
+
+#define TRXN_DATA_SZ1  (TRXN_BEACON_SZ+TRXN_SENDER_SZ+TRXN_RECEIVER_SZ+TRXN_AMOUNT_SZ)
+#define TRXN_DATA_SZ2  (TRXN_TIMESTAMP_SZ+TRXN_TRXNTYPE_SZ+TRXN_ASSETTYPE_SZ)
+#define TRXN_DATA_SZ   (TRXN_DATA_SZ1 + TRXN_DATA_SZ2)
+
+#define TRXN_BODY_SZ  (TRXN_HEADER_SZ+TRXN_DATA_SZ)
+#define TRXN_TOTAL_SZ (TRXN_DATA_SZ+TRXN_SIGNATURE_SZ)
+
 class OmicroTrxn
 {
   public:
   	OmicroTrxn();
+  	OmicroTrxn(const char *str);
   	~OmicroTrxn();
 
-	char header[6];
-	char sender[503];
-	char receiver[503];
-	char amount[16];   // "123456789.123456"
-	char signature[64];
-	char beacon[8];
-	char timestamp[16];  // "123456789012.124"
-	char trxnType[2];
-	char assetType[2];
+	char *getHeader();
+	bool setHeader( const char *s);
+
+	char *getBeacon();
+	bool setBeacon( const char *s);
+
+	char *getSender();
+	bool setSender( const char *s );
+
+	char *getReceiver();
+	bool setReceiver( const char *s );
+
+	char *getAmount();
+	double getAmountDouble();
+	bool setAmount( const char *s );
+
+	char *getTimeStamp();
+	bool setTimeStamp( const char *s );
+
+	char *getTrxnType();
+	bool setTrxnType( const char *s );
+
+	char *getAssetType();
+	bool setAssetType( const char *s );
+
+	char *getSignature();
+	bool setSignature( const char *s );
+
+	char *getString();
+
 
   protected:
-
+	char data_[TRXN_TOTAL_SZ+1];
+  	/*** Inside data_:
+	header_[TRXN_HEADER_SZ];
+	beacon_[TRXN_BEACON_SZ];
+	sender_[TRXN_SENDER_SZ];
+	receiver_[TRXN_RECEIVER_SZ];
+	amount_[TRXN_AMOUNT_SZ];   // "123456789.123456"
+	timestamp_[TRXN_TIMESTAMP_SZ];  // "123456789012.124"
+	trxnType_[TRXN_TRXNTYPE_SZ];
+	assetType_[TRXN_ASSETTYPE_SZ];
+	signature_[TRXN_SIGNATURE_SZ];
+	***/
 };
 
 #endif
