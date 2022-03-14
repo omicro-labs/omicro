380b1e0753e42f9387fa8fb754a02f3a5d7c00a2
wasabiomicro <wasabiomicro@omicro>
2022-03-14 12:28:32 -0700

    omicrokey.cc

--- src/omicrokey.cc
+++ src/omicrokey.cc
@@ -9,18 +9,30 @@ OmicroKey::~OmicroKey()
 }
 
 
-int OmicroKey::createKeyPair( const sstr &salt, sstr &secretKey, sstr &publicKey )
+int OmicroKey::createKeyPair(int level, const sstr &salt, sstr &secretKey, sstr &publicKey )
 {
 	return 0;
 }
 
-sstr OmicroKey::encrypt( const sstr &msg )
+sstr OmicroKey::encrypt( int level, const sstr &msg,  const sstr &secretKey )
 {
 	return "";
 }
 
-sstr OmicroKey::decrypt( const sstr &msg )
+sstr OmicroKey::decrypt( int level, const sstr &encmsg, const sstr &publicKey )
 {
 	return "";
 }
+
+
+sstr OmicroKey::sign( int level, const sstr &msg, const sstr &secretKey )
+{
+	return "";
+}
+
+bool OmicroKey::verify(int level, const sstr &signedmsg, const sstr &publicKey )
+{
+	return true;
+}
+
 
--- src/omicrokey.h
+++ src/omicrokey.h
@@ -2,6 +2,7 @@
 #define _omicro_key_h_
 
 #include "omicrodef.h"
+#include "src/ntru.h"
 
 class OmicroKey
 {
@@ -9,9 +10,13 @@ class OmicroKey
   	OmicroKey();
   	~OmicroKey();
 
-	int createKeyPair( const sstr &salt, sstr &secretKey, sstr &publicKey );
-	sstr encrypt( const sstr &msg ); 
-	sstr decrypt( const sstr &msg ); 
+	// level: 1 moderate; 2 high;  3 highest
+	int createKeyPair( int level, const sstr &salt, sstr &secretKey, sstr &publicKey );
+	sstr encrypt( int level, const sstr &msg, const sstr &secretKey ); 
+	sstr decrypt( int level, const sstr &encmsg, const sstr &publicKey ); 
+
+	sstr sign( int level, const sstr &msg, const sstr &secretKey );
+	bool verify( int level, const sstr &signedmsg, const sstr &publicKey );
 
 };
 
