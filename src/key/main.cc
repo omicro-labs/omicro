7688906ce7cfb6109256730bec8a11c8c3581de2
wasabiomicro <wasabiomicro@omicro>
2022-04-23 11:49:30 -0700

    pulled openssl3.0.2 and use its static lib

--- src/key/main.cc
+++ src/key/main.cc
@@ -4,13 +4,14 @@
 #include <omutil.h>
 #include <omicrodef.h>
 #include <omicrokey.h>
+#include <omlog.h>
 
 INIT_LOGGING
 
 int main(int argc, char* argv[])
 {
 	sstr sk, pk;
-	OmicroKey::createKeyPair( sk, pk);
+	OmicroNodeKey::createKeyPairSB3( sk, pk);
 	printf("%s %s\n", s(sk), s(pk) );
     return 0;
 }
